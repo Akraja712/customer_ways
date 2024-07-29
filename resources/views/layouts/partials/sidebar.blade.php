8eb946b507b05d3aa023ea826bb7d0cf517589e9
Akraja712 <ashokkumar7hitter@gmail.com>
2024-07-29 16:27:02 +0530

    index.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -92,11 +92,11 @@
                 </li>
 
                 <li class="nav-item has-treeview">
-    <a href="{{ route('appsettings.edit', 1) }}" class="nav-link {{ activeSegment('appsettings') }}">
-        <i class="nav-icon fas fa-gear"></i>
-        <p>App Settings</p>
-    </a>
-</li>
+                    <a href="{{ route('appsettings.edit', 1) }}" class="nav-link {{ activeSegment('appsettings') }}">
+                        <i class="nav-icon fas fa-gear"></i>
+                        <p>App Settings</p>
+                    </a>
+                </li>
 
 
                
